686234285a12ef551990e1a0a6d44a41f254c8c4
Monika Synowiec <monika.synowiec@motorolasolutions.com>
2018-06-02 15:01:48 +0200

    initial commit

---
+++ js/ui.js
@@ -0,0 +1,58 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        $('body').removeClass('menuopen');
+        return false;
+      }
+    }
+  });
+
+  $('.navbar-toggler').click(function(){
+    if ($('#navbarResponsive').hasClass('show')) {
+      document.querySelector('body').classList.remove('menuopen');
+    } else {
+      document.querySelector('body').classList.add('menuopen');
+    }   
+});
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+    // Magnific popup calls
+    $('.popup-gallery').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
+  });;
+
+})(jQuery); // End of use strict
